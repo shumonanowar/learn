09210b660752a5a551f9545e96697079cda454bf
Shumon Anowar <Shumon.Anowar@welltok.com>
2015-11-10 21:42:23 -0700

    6th exercise

--- ex6.rb
+++ ex6.rb
@@ -13,7 +13,7 @@ puts "I also said: '#{y}'."
 hilarious = false
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
-puts joke_evaluation
+puts "joke_evaluation"
 
 w = "This is the left side of..."
 e = "a string with a right side."
