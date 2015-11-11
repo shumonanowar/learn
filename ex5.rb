d296a57fc6e6e440103eda822d762564d3f06b36
Shumon Anowar <Shumon.Anowar@welltok.com>
2015-11-10 21:06:36 -0700

    5th exercise

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Shumon Anowar'
+my_age = 35 # not a lie
+my_height = 70 # inches
+my_weight = 167 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} and #{my_hair}"
+puts "His teeth are usually #{my_teeth} depending on coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight}, I get #{my_age + my_weight} "
