7452d62588aa700b7a43ab7e7c28547569030d5a
Shumon Anowar <Shumon.Anowar@welltok.com>
2015-11-11 09:08:38 -0700

    11th exercise

--- ex11.rb
+++ ex11.rb
@@ -1,8 +1,14 @@
 print "give me a number: "
-number = gets.chomp.to_i
+number = gets.chomp.to_f
 
 bigger = number * 100
 puts "A bigger number is #{bigger}"
+bigger2 = bigger.to_i
+puts "A bigger number integer #{bigger2}"
+
 
 smaller = number/100
+puts "A smaller number is #{smaller.to_i}"
+
+smaller = number.to_f/100
 puts "A smaller number is #{smaller}"
