1e0569f5c1d1cc606a8d7dbe86edaa17fdb8bd6c
Shumon Anowar <Shumon.Anowar@welltok.com>
2015-11-10 21:32:26 -0700

    6th exercise

--- ex5.rb
+++ ex5.rb
@@ -1,7 +1,7 @@
 my_name = 'Shumon Anowar'
 my_age = 35 # not a lie
-my_height = 70 # inches
-my_weight = 167 # lbs
+my_height = 74 # inches
+my_weight = 180 # lbs
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Black'
@@ -13,4 +13,4 @@ puts "Actually that's not too heavy."
 puts "He's got #{my_eyes} and #{my_hair}"
 puts "His teeth are usually #{my_teeth} depending on coffee."
 
-puts "If I add #{my_age}, #{my_height}, and #{my_weight}, I get #{my_age + my_weight} "
+puts "If I add #{my_age}, #{my_height}, and #{my_weight}, I get #{my_age + my_height + my_weight} "
