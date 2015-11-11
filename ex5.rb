3c0b10b7c7118869e747bb0afd80aa41fa03174b
Shumon Anowar <Shumon.Anowar@welltok.com>
2015-11-10 21:40:41 -0700

    5th exercise

--- ex5.rb
+++ ex5.rb
@@ -12,5 +12,4 @@ puts "He's #{my_weight} pounds heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{my_eyes} and #{my_hair}"
 puts "His teeth are usually #{my_teeth} depending on coffee."
-
 puts "If I add #{my_age}, #{my_height}, and #{my_weight}, I get #{my_age + my_height + my_weight} "
